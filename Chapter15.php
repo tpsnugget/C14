1f1f80ffc8976143f8f096093125c44715a445ab
Michael Giebner <mikegiebner@gmail.com>
2020-02-29 16:56:22 -0500

    Chapter 15 complete

---
+++ Chapter15.php
@@ -0,0 +1,53 @@
+<?php
+
+   abstract class Animal{
+
+      public $name;
+      public $color;
+
+      public function describe(){
+         return $this->name . " is " . $this->color;
+      }
+
+      abstract public function makeSound();
+
+   }
+
+   class Duck extends Animal{
+
+      public function describe(){
+         return parent::describe();
+      }
+
+      public function makeSound(){
+         return "Quack";
+      }
+
+   }
+
+   class Dog extends Animal{
+
+      public function describe(){
+         return parent::describe();
+      }
+
+      public function makeSound(){
+         return "Woof";
+      }
+
+   }
+
+   $duck = new Duck;
+   $duck->name = "Ben";
+   $duck->color = "Yellow";
+
+   echo $duck->describe() . "\n";
+   echo $duck->makeSound() . "\n";
+
+   $dog = new Dog;
+   $dog->name = "Olive";
+   $dog->color = "black";
+   echo $dog->describe() . "\n";
+   echo $dog->makeSound() . "\n";
+
+?>
--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
       private $name;
 
-      // public function __set($name, $value){
-      //    echo "Setting " . $name . " to <strong>" . $value . "</strong><br />";
-      //    $this->name = $value;
-      // }
+      public function __set($name, $value){
+         echo "Setting " . $name . " to <strong>" . $value . "</strong>\n";
+         $this->name = $value;
+      }
 
       public function __get($name){
-         echo "Getting " . $name . "<strong> " . $this->name . "</strong><br />";
+         echo "Getting " . $name . "<strong> " . $this->name . "</strong>\n";
       }
 
       // public function __isset($name){
@@ -23,7 +23,7 @@
    $post = new Post;
    $post->name = "Testing";
    $getName = $post->name;
-   echo $getName;
+   echo $getName . "\n";
    // var_dump(isset($post->name));
 
 ?>
