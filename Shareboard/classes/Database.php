35888735f2bab93b5543f9cd9feb6cad3c0958c5
Michael Giebner <mikegiebner@gmail.com>
2020-03-01 12:36:22 -0500

    Chapter20 complete

--- Shareboard/classes/Database.php
+++ Shareboard/classes/Database.php
@@ -24,8 +24,50 @@
          } catch(PDOException $e){
             print "Error!: " . $this->error = $e->getMessage() . "\n";
          }
-
       }
+
+      public function query($query){
+         $this->statement = $this->dbHandler->prepare($query);
+      }
+
+      public function bind($param, $value, $type = null){
+         if(is_null($type)){
+            switch(true){
+               case is_int($value):
+               $type = PDO::PARAM_INT;
+               break;
+
+               case is_bool($value):
+               $type = PDO::PARAM_BOOL;
+               break;
+
+               case is_null($value):
+               $type = PDO::PARAM_NULL;
+               break;
+               default:
+               $type = PDO::PARAM_STRING;
+            }
+         }
+         $this->statement->bindValue($param, $value, $type);
+      }
+
+      public function execute(){
+         return $this->statement->execute();
+      }
+
+      public function resultSet(){
+         $this->execute();
+         return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+      // public function __set($name, $value){
+      //    $this->name = $value;
+      // }
+
+      // public function __get($name){
+      //    print "Getting '$name' \n";
+      //    return $this->name;
+      // }
 
    }
 
